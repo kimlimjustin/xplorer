c5e52e3bc1d84c0d2cc0c96a6fa9ad4a3e9b689f
kimlimjustin <kimlimjustin@gmail.com>
2021-04-24 21:50:23 +0700

    Make app frameless window

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,36 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
 
-function createWindow () {
-  const win = new BrowserWindow({
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+try {
+   require('electron-reloader')(module)
+} catch (_) { }
 
-  win.loadFile('src/index.html')
-  Menu.setApplicationMenu(menu)
+function createWindow() {
+   const win = new BrowserWindow({
+      transparent:true,
+      frame: false,
+      webPreferences: {
+         preload: path.join(__dirname, 'preload.js'),
+         enableRemoteModule: true
+      }
+   })
+
+   win.loadFile('src/public/index.html')
+   //win.removeMenu()
 }
 
 app.whenReady().then(() => {
-  createWindow()
+   createWindow()
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
+   app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+         createWindow()
+      }
+   })
 })
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+   if (process.platform !== 'darwin') {
+      app.quit()
+   }
 })
--- src/preload.js
+++ src/preload.js
@@ -1,9 +1,34 @@
 const fs = require('fs');
+const {remote} = require('electron');
+
 window.addEventListener('DOMContentLoaded', () => {
     const theme = JSON.parse(fs.readFileSync("src/config/theme.json"));
+    // Styling
     document.body.style.backgroundColor =  theme.default.mainBackground
     document.body.style.color = theme.default.textColor
     document.body.style.fontSize = theme.default.fontSize
+    document.querySelector(".tabs-manager").style.backgroundColor = theme.default.tabsManager
     document.querySelector(".sidebar").style.backgroundColor = theme.default.sidebarBackground
+    document.querySelector("#minimize").style.backgroundColor = theme.default.minimizeBackgroundColor
+    document.querySelector("#minimize").style.color = theme.default.minimizeColor
+    document.querySelector("#maximize").style.backgroundColor = theme.default.maximizeBackgroundColor
+    document.querySelector("#maximize").style.color = theme.default.maximizeColor
+    document.querySelector("#exit").style.backgroundColor = theme.default.exitBackgroundColor
+    document.querySelector("#exit").style.color = theme.default.exitColor
+
+    // Minimize the screen
+    document.querySelector("#minimize").addEventListener("click", () => {
+        const electronWindow = remote.BrowserWindow.getFocusedWindow()
+        electronWindow.minimize()
+    })
+    // Maximize the screen
+    document.querySelector("#maximize").addEventListener("click", () => {
+        const electronWindow = remote.BrowserWindow.getFocusedWindow()
+        !electronWindow.isMaximized() ? electronWindow.maximize() : electronWindow.unmaximize()
+    })
+    document.querySelector("#exit").addEventListener("click", () => {
+        const electronWindow = remote.BrowserWindow.getFocusedWindow()
+        electronWindow.close()
+    })
 })
   
