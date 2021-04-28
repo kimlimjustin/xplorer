a168fedc728cdf2dc7ac57ef9d95245a5772eb96
kimlimjustin <kimlimjustin@gmail.com>
2021-04-28 23:13:35 +0700

    Make favorites as a component

---
+++ src/Components/favorites.ts
@@ -0,0 +1,27 @@
+const path = require('path')
+const fs = require('fs');
+const Favorites = () => {
+    const iconJSON = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../config/icon.json")));
+    return(
+        `<h2 class="favorites-title">Favorites</h2>
+        <div class="favorite">
+            <h3 class="favorite-title"><img src="${path.join(__dirname,  `../icon/${iconJSON.favorites.desktop}`)}" alt="Desktop icon" class="favorite-icon">Desktop</h3>
+        </div>
+        <div class="favorite">
+            <h3 class="favorite-title"><img src="${path.join(__dirname,  `../icon/${iconJSON.favorites.document}`)}" alt="Document icon" class="favorite-icon">Documents</h3>
+        </div>
+        <div class="favorite">
+            <h3 class="favorite-title"><img src="${path.join(__dirname,  `../icon/${iconJSON.favorites.download}`)}" alt="Download icon" class="favorite-icon">Downloads</h3>
+        </div>
+        <div class="favorite">
+            <h3 class="favorite-title"><img src="${path.join(__dirname,  `../icon/${iconJSON.favorites.picture}`)}" alt="Pictures icon" class="favorite-icon">Pictures</h3>
+        </div>
+        <div class="favorite">
+            <h3 class="favorite-title"><img src="${path.join(__dirname,  `../icon/${iconJSON.favorites.music}`)}" alt="Music icon" class="favorite-icon">Music</h3>
+        </div>
+        <div class="favorite">
+            <h3 class="favorite-title"><img src="${path.join(__dirname,  `../icon/${iconJSON.favorites.video}`)}" alt="Video icon" class="favorite-icon">Videos</h3>
+        </div>`
+    )
+}
+module.exports =  Favorites
