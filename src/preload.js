8fe01acf067482b72b25fd4f15540e68b4f0cf48
kimlimjustin <kimlimjustin@gmail.com>
2021-04-30 20:58:17 +0700

    Updated tabs and navigator

--- src/preload.js
+++ src/preload.js
@@ -50,7 +50,8 @@ document.addEventListener('DOMContentLoaded', () => {
         closeTab.innerHTML = "&times;"
         closeTab.classList.add("close-tab-btn");
         // Listen to close tab button
-        closeTab.addEventListener("click", () => {
+        closeTab.addEventListener("click", e => {
+            e.stopPropagation()
             // Close the window if user close the only tab
             if(document.querySelectorAll(".tab").length === 1){
                 const electronWindow = remote.BrowserWindow.getFocusedWindow()
@@ -60,16 +61,25 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
         tab.appendChild(closeTab)
+
+        // Scroll the tab
+        tab.addEventListener("click",() => {
+            // Check whether the tab is scrolling to left or to right
+            if(tab.parentNode.scrollLeft < window.pageXOffset + tab.getBoundingClientRect().left){
+                tab.parentNode.scrollLeft = window.pageXOffset + tab.getBoundingClientRect().left + tab.offsetWidth
+            }else tab.parentNode.scrollLeft = window.pageXOffset + tab.getBoundingClientRect().left - tab.offsetWidth
+        })
     })
-    
-    // Create a new tab event
+
     const createNewTabElement = document.querySelector(".create-new-tab")
-    createNewTabElement.addEventListener('click', ()=> {
+    //  Function to create new tab
+    const createNewTab = e => {
         const newTab = document.createElement("div");
         newTab.classList.add('tab');
         newTab.innerHTML = "Home<span class='close-tab-btn'>&times;</span>";
         // Listen to close tab button
-        newTab.querySelector(".close-tab-btn").addEventListener("click", () => {
+        newTab.querySelector(".close-tab-btn").addEventListener("click", e => {
+            e.stopPropagation()
             // Close the window if user close the only tab
             if(document.querySelectorAll(".tab").length === 1){
                 const electronWindow = remote.BrowserWindow.getFocusedWindow()
@@ -79,8 +89,27 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
         createNewTabElement.parentElement.insertBefore(newTab, createNewTabElement) // Insert the new tab
+        newTab.parentNode.scrollLeft = window.pageXOffset + newTab.getBoundingClientRect().left + newTab.offsetWidth // Scroll the tabs scrollbar
         updateTheme() // Update the theme
-    })
+
+        // Scroll the tab
+        newTab.addEventListener("click",() => {
+            // Check whether the tab is scrolling to left or to right
+            if(newTab.parentNode.scrollLeft < window.pageXOffset + newTab.getBoundingClientRect().left){
+                newTab.parentNode.scrollLeft = window.pageXOffset + newTab.getBoundingClientRect().left + newTab.offsetWidth
+            }else newTab.parentNode.scrollLeft = window.pageXOffset + newTab.getBoundingClientRect().left - newTab.offsetWidth
+        })
+    }
+    
+    // Create a new tab event
+    createNewTabElement.addEventListener('click',createNewTab)
+    // Add new tab shortcut
+    const shortcut = e => {
+        if(e.ctrlKey && e.key === "t"){
+            createNewTab()
+        }
+    }
+    document.addEventListener("keyup", shortcut, false)
 
     // Function to update page theme
     const updateTheme = () => {
