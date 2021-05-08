a87527d5b620b35f5327f7b6533f98308a39a0d0
kimlimjustin <kimlimjustin@gmail.com>
2021-05-08 15:35:08 +0700

    Displaying file as icon if the file is image and added some icons

--- src/Functions/Icon/icon.js
+++ src/Functions/Icon/icon.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const IMAGE = ['jpg', 'png', 'gif', 'bmp', 'jpeg', 'jpe', 'jif', 'jfif', 'jfi', 'webp', 'tiff', 'tif', 'ico'];
+
 const getIcon = async (filename, isDir) => {
     const iconJSON = await fs.readFileSync(path.join(__dirname, "../../config/icon.json"))
     if(isDir){
@@ -10,6 +12,7 @@ const getIcon = async (filename, isDir) => {
         }else return path.join(__dirname, "../../icon/", JSON.parse(iconJSON).default.folder)
     }else{
         const ext = filename.split('.').pop(); // Get extension of filename
+        if(IMAGE.indexOf(ext) !== -1) return filename // Show the image itself if the file is image
         // Check if there's a icon for the filename name
         if(Object.keys(JSON.parse(iconJSON).extension).indexOf(ext) !== -1){
             return path.join(__dirname, "../../icon/", JSON.parse(iconJSON).extension[ext])
