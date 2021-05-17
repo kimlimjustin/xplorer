a45a0bd26ffa53fc0884e0c40620734249dfcf42
kimlimjustin <kimlimjustin@gmail.com>
2021-05-17 22:11:24 +0700

    Added drive into sidebar

--- src/Functions/DOM/sidebar.js
+++ src/Functions/DOM/sidebar.js
@@ -1,6 +1,5 @@
-const path = require("path");
-const storage = require('electron-json-storage-sync')
-const fs = require('fs');
+const storage = require('electron-json-storage-sync');
+const { getDrives } = require('../../Components/drives');
 const getPreview = require("../preview/preview");
 
 const changeSidebar = newElement => {
@@ -18,6 +17,26 @@ const Sidebar = () => {
         return result;
     }
 
+    // Functions to get and display drives on sidebar
+    const getDrivesElement = async () => {
+        const drives = await getDrives()
+        if(!drives.length || process.platform === "darwin") return "" // Return nothing if there's no drives detected or its running on macOS
+        else{
+            let drivesElement = ""
+            for(const drive of drives){
+                let driveName = process.platform === "win32" ? `${drive._volumename} (${drive._mounted})`: drive._mounted.split("/")[drive._mounted.split("/").length - 1] // Get name of drive
+                drivesElement += `<span><img src="${getPreview('usb', category = 'favorites', HTMLFormat = false)}" alt="${driveName}">${driveName}</span>`
+            }
+            let result = `<div class="sidebar-nav-item">
+                <span class="sidebar-nav-item-dropdown-btn"><img src="${getPreview('usb', category = "favorites", HTMLFormat = false)}" alt="Drives icon"> ${process.platform === "win32" ? "Drives" : "Pendrives"}</span>
+                <div class="sidebar-nav-item-dropdown-container">
+                    ${drivesElement}
+                </div>
+            </div>`
+            return result;
+        }
+    }
+
     const {data} = storage.get('sidebar'); // Get user favorites data on sidebar
 
     let _favorites = ['Home', 'Recent', 'Desktop', 'Documents', 'Downloads', 'Pictures', 'Music', 'Videos', 'Trash']
@@ -30,24 +49,29 @@ const Sidebar = () => {
     }
     let sidebarElement = document.createElement("div");
     sidebarElement.classList.add("sidebar")
-    sidebarElement.innerHTML =  `
-    <span class="xplorer-brand">Xplorer</span>
-    <div class="sidebar-nav">
-        <div class="sidebar-nav-item">
-            <span class="sidebar-nav-item-dropdown-btn"><img src="${getPreview('Favorites', category = "sidebar", HTMLFormat = false)}" alt="Favorites icon"> Favorites</span>
-            <div class="sidebar-nav-item-dropdown-container">
-                ${favoritesElement(_favorites)}
-            </div>
-        </div>
-    </div>
-    <div class="sidebar-setting-btn">
-        <img src="${getPreview('setting', category= 'sidebar', HTMLFormat = false)}" alt="Setting icon" class="sidebar-setting-btn-icon">
-        <span class="sidebar-setting-btn-text">Settings</span>
-    </div>`
 
-    sidebarElement.querySelectorAll(".sidebar-nav-item-dropdown-btn").forEach(btn => {
-        btn.addEventListener("click", e => {
-            e.target.parentNode.classList.toggle('nav-hide-item')
+    getDrivesElement().then(drivesElement => { // get drives element
+        sidebarElement.innerHTML =  `
+        <span class="xplorer-brand">Xplorer</span>
+        <div class="sidebar-nav">
+            <div class="sidebar-nav-item">
+                <span class="sidebar-nav-item-dropdown-btn"><img src="${getPreview('Favorites', category = "sidebar", HTMLFormat = false)}" alt="Favorites icon"> Favorites</span>
+                <div class="sidebar-nav-item-dropdown-container">
+                    ${favoritesElement(_favorites)}
+                </div>
+            </div>
+            ${drivesElement}
+        </div>
+        <div class="sidebar-setting-btn">
+            <img src="${getPreview('setting', category= 'sidebar', HTMLFormat = false)}" alt="Setting icon" class="sidebar-setting-btn-icon">
+            <span class="sidebar-setting-btn-text">Settings</span>
+        </div>`
+
+        // Collapse section
+        sidebarElement.querySelectorAll(".sidebar-nav-item-dropdown-btn").forEach(btn => {
+            btn.addEventListener("click", e => {
+                e.target.parentNode.classList.toggle('nav-hide-item')
+            })
         })
     })
 
