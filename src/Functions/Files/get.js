9c0d82c59fc2921c75b15b36c04be80d897d0507
kimlimjustin <kimlimjustin@gmail.com>
2021-05-21 22:47:19 +0700

    Exit `fs.watch` from a folder when xplorer exit that folder

--- src/Functions/Files/get.js
+++ src/Functions/Files/get.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const filterHidden = require('../Filter/hiddenFiles');
+const storage = require("electron-json-storage-sync")
 
 const IGNORE_FILE = ['desktop.ini']
 
@@ -13,7 +14,7 @@ const getFilesAndDir = async (dir, callback) => {
         callback(files)
     })
     // Watch the directory
-    fs.watch(dir, async (eventType, filename) => {
+    const watcher = fs.watch(dir, async (eventType, filename) => {
         // Get files of the dir
         const files = await fs.readdirSync(dir, { withFileTypes: true }).map(dirent => { return { "filename": dirent.name, "isDir": dirent.isDirectory() } })
         // Filter hidden files
@@ -21,6 +22,19 @@ const getFilesAndDir = async (dir, callback) => {
             callback(files)
         })  
     })
+
+    let focusingPath; // Watch if focusing path changes
+    setInterval(() => {
+        const tabs = storage.get('tabs')?.data
+        const _focusingPath = tabs.tabs[tabs.focus]
+        if (focusingPath === undefined) {
+            focusingPath = _focusingPath
+        } else {
+            if (focusingPath !== _focusingPath) {
+                watcher.close()
+            }
+        }
+    }, 500);
 }
 
 module.exports = { getFilesAndDir }
