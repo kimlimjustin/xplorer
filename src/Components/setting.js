19073bd7f4760679a277dc5d6ef0d99443b89997
kimlimjustin <kimlimjustin@gmail.com>
2021-06-14 22:55:48 +0700

    feat: add preference setting

--- src/Components/setting.js
+++ src/Components/setting.js
@@ -3,10 +3,12 @@ const getPreview = require("../Functions/preview/preview");
 const { updateTheme } = require("../Functions/Theme/theme");
 const storage = require("electron-json-storage-sync");
 const theme = storage.get("theme")?.data?.theme
+const fs = require("fs");
+const path = require("path");
 
 const Appearance = () => {
     let settingsMain = document.querySelector(".settings-main");
-    settingsMain.innerHTML = `<h3>App Theme</h3>
+    settingsMain.innerHTML = `<h3 class="settings-title">App Theme</h3>
     <select>
         <option>System Default</option>
         <option ${theme === "light" ? "selected" : ""} value="light">Light</option>
@@ -22,12 +24,44 @@ const Appearance = () => {
 }
 
 const Preference = () => {
+    const language = storage.get("preference")?.data?.language
+    const hideHiddenFiles = storage.get("preference")?.data?.hideHiddenFiles
     let settingsMain = document.querySelector(".settings-main");
-    settingsMain.innerHTML = `<h3></h3>`
+    const availableLanguages = JSON.parse(fs.readFileSync(path.join(__dirname, "../Languages/index.json")))?.availableLanguages
+    settingsMain.innerHTML = `<h3 class="settings-title">App Language</h3>
+    <select name="language">
+    ${Object.keys(availableLanguages).map(lang => {
+        return `<option value="${availableLanguages[lang]}" ${language === availableLanguages[lang] ? "selected" : ""}>${lang}</option>`
+    })}
+    </select>
+    <h3 class="settings-title">Files and Folders</h3>
+    <div class="toggle-box">
+        <label class="toggle">
+            <input type="checkbox" name="hide-hidden-files" ${hideHiddenFiles ? "checked" : ""}>
+            <span class="toggle-slider"></span>
+            <span class="toggle-label">Hide Hidden Files</span>
+        </label>
+    </div>
+`
+    settingsMain.querySelector(`[name="language"]`).addEventListener("change", ({ target: { value } }) => {
+        let preference = storage.get("preference")?.data
+        preference.language = value
+        storage.set("preference", preference)
+        const { reload } = require("./windowManager");
+        reload()
+    })
+    settingsMain.querySelector(`[name="hide-hidden-files"]`).addEventListener("change", ({ target: { checked } }) => {
+        let preference = storage.get("preference")?.data
+        preference.hideHiddenFiles = checked
+        storage.set("preference", preference)
+        document.getElementById("main").dataset.hideHiddenFiles = checked
+        document.getElementById("show-hidden-files").checked = !checked
+    })
 }
 
 const About = () => {
-
+    let settingsMain = document.querySelector(".settings-main");
+    settingsMain.innerHTML = `<h3></h3>`
 }
 
 const Setting = () => {
