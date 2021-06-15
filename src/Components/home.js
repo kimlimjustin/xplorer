4d2760e50647e707657e16776beb916532a20967
kimlimjustin <kimlimjustin@gmail.com>
2021-06-15 22:48:29 +0700

    fix: file animation not working on linux bug

--- src/Components/home.js
+++ src/Components/home.js
@@ -18,7 +18,7 @@ const homeFiles = (callback) => {
         let result = `<section class='home-section'><h1 class="section-title">Files</h1>`;
         for (const file of files) {
             const preview = await getPreview(path.join(os.homedir(), file.filename), category = file.isDir ? "folder" : "file")
-            result += `<div class="file-grid file-hover-effect" draggable="true" data-isdir=${file.isDir} data-path = "${escape(path.join(os.homedir(), file.filename))}" data-listenOpen ${file.isHidden ? "data-hidden-file" : ""}>
+            result += `<div class="file-grid grid-hover-effect" draggable="true" data-isdir=${file.isDir} data-path = "${escape(path.join(os.homedir(), file.filename))}" data-listenOpen ${file.isHidden ? "data-hidden-file" : ""} data-tilt>
             ${preview}
             <span class="file-grid-filename" id="file-filename">${Translate(file.filename)}</span>
             </div>`
