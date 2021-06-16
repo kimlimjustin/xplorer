504d5e581c27a200dd0b0c49ae5bf79105807389
kimlimjustin <kimlimjustin@gmail.com>
2021-06-16 23:27:35 +0700

    test: remove readDir testing

--- test/wasm.test.js
+++ test/wasm.test.js
@@ -1,15 +1,10 @@
-const { readDir, extractIcon } = require("../lib/wasm/bindings");
-const path = require('path');
+const { extractIcon } = require("../lib/wasm/bindings");
 const fs = require('fs');
 
-test('read files in directory', () => {
-    expect(readDir(__dirname)[process.platform === "win32" ? 2 : 0].filename).toBe(path.basename(__filename))
-})
-
-if (process.platform === "win32") {
-    test('Extract exe icon', () => {
+test('Extract exe icon', () => {
+    if (process.platform === "win32") {
         const buffer = extractIcon("C:\\Windows\\System32\\cmd.exe", "large");
         fs.writeFileSync("test.ico", buffer);
         fs.unlinkSync("test.ico")
-    })
-}
+    }
+})
