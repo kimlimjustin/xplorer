021946732cf99f712ac04ad3d3946ba5a06323bc
kimlimjustin <kimlimjustin@gmail.com>
2021-06-20 20:54:41 +0700

    test: add test for format bytes and remove node version 14 from github ci

---
+++ test/math.test.js
@@ -0,0 +1,10 @@
+const formatBytes = require("../src/Functions/Math/filesize");
+
+test("Format Bytes", () => {
+    expect(formatBytes(1024)).toBe("1 KB")
+    expect(formatBytes(102)).toBe("102 Bytes")
+    expect(formatBytes(2097052)).toBe("2 MB")
+    expect(formatBytes(1073741824)).toBe("1 GB")
+    expect(formatBytes(1073741824)).toBe("1 GB")
+    expect(formatBytes(6356551598)).toBe("5.92 GB")
+})
