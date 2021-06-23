cb0382deecc572cd47e2e30c33b56a452a7c238d
kimlimjustin <kimlimjustin@gmail.com>
2021-06-23 22:20:17 +0700

    feat: add layout mode and sort into recent folder

--- src/Components/recent.js
+++ src/Components/recent.js
@@ -2,15 +2,28 @@ const { startLoading, stopLoading } = require("../Functions/DOM/loading");
 const storage = require('electron-json-storage-sync');
 const getPreview = require("../Functions/preview/preview");
 const LAZY_LOAD = require("../Functions/DOM/lazyLoadingImage");
+const { updateTheme } = require("../Functions/Theme/theme");
+const getType = require("../Functions/Files/type");
 
 const Recent = async () => {
     startLoading()
+    // Preference data
+    const layout = storage.get("layout")?.data?.["Recent"] ?? storage.get("preference")?.data?.layout ?? "s"
+    const sort = storage.get("sort")?.data?.["Recent"] ?? 'A'
     // Get the main element
     const MAIN_ELEMENT = document.getElementById("main");
     MAIN_ELEMENT.innerHTML = "";
     if (MAIN_ELEMENT.classList.contains('empty-dir-notification')) MAIN_ELEMENT.classList.remove('empty-dir-notification') // Remove class if exist
     // Get recent files list
-    const recents = storage.get('recent')?.data
+    let recents = storage.get('recent')?.data
+    recents = recents.sort((a, b) => {
+        switch (sort) {
+            case "A": // A-Z
+                return a.split('\\').pop().split('/').pop().toLowerCase() > b.split('\\').pop().split('/').pop().toLowerCase() ? 1 : -1
+            case "Z": // Z-A
+                return a.split('\\').pop().split('/').pop().toLowerCase() < b.split('\\').pop().split('/').pop().toLowerCase() ? 1 : -1
+        }
+    })
     if (!recents) {
         MAIN_ELEMENT.classList.add('empty-dir-notification')
         MAIN_ELEMENT.innerText = "This folder is empty."
@@ -18,7 +31,22 @@ const Recent = async () => {
         for (const recent of recents) {
             const preview = await getPreview(recent, category = "file")
             const fileGrid = document.createElement("div")
-            fileGrid.className = "file-grid grid-hover-effect"
+            fileGrid.className = "file-grid file grid-hover-effect"
+            switch (layout) {
+                case "m":
+                    fileGrid.classList.add("medium-grid-view")
+                    break;
+                case "l":
+                    fileGrid.classList.add("large-grid-view")
+                    break;
+                case "d":
+                    fileGrid.classList.add("detail-view")
+                    break;
+                default:
+                    fileGrid.classList.add("small-grid-view")
+                    break;
+
+            }
             fileGrid.setAttribute("draggable", 'true')
             fileGrid.setAttribute("data-listenOpen", '')
             fileGrid.setAttribute("data-tilt", '')
@@ -26,10 +54,12 @@ const Recent = async () => {
             fileGrid.innerHTML = `
             ${preview}
             <span class="file-grid-filename" id="file-filename">${recent.split('\\').pop().split('/').pop()}</span>
+            <span class="file-type">${getType(recent)}</span>
             `
             MAIN_ELEMENT.appendChild(fileGrid)
         }
         LAZY_LOAD()
+        updateTheme()
     }
     stopLoading()
 }
