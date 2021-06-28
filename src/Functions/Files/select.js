0f8900874316326b16430d6911a9fdf46aa4463e
kimlimjustin <kimlimjustin@gmail.com>
2021-06-28 22:36:16 +0700

    refactor: multiple grid selection using shift algorithm to fix some bugs

--- src/Functions/Files/select.js
+++ src/Functions/Files/select.js
@@ -1,6 +1,7 @@
 const storage = require("electron-json-storage-sync");
 const { isHiddenFile } = require("is-hidden-file");
 let latestSelected;
+let latestShiftSelected;
 let initialized = false;
 /**
  * Select a file grid...
@@ -37,7 +38,10 @@ const Select = (element, ctrl, shift, elements) => {
             }
         }
     }
-    else latestSelected = element
+    else {
+        latestSelected = element
+        latestShiftSelected = element
+    }
 }
 
 /**
@@ -57,33 +61,58 @@ const Initializer = () => {
     const selectShortcut = (e) => {
         const hideHiddenFiles = storage.get("preference")?.data?.hideHiddenFiles ?? true
         if (e.key === "ArrowRight") {
+            if (Array.from(latestSelected.parentNode.children).indexOf(latestShiftSelected) < Array.from(latestSelected.parentNode.children).indexOf(latestSelected)) latestShiftSelected = latestSelected
             if (!document.contains(latestSelected)) { selectFirstFile(); return; }
             e.preventDefault()
-            let nextSibling = latestSelected.nextSibling;
+            let nextSibling = e.shiftKey ? latestShiftSelected.nextSibling : latestSelected.nextSibling;
             if (hideHiddenFiles) {
                 while (nextSibling && nextSibling.dataset.hiddenFile !== undefined) {
                     nextSibling = nextSibling.nextSibling
                 }
             }
             if (nextSibling?.className.split(' ').some(function (c) { return /file/.test(c); })) {
-                if (!e.shiftKey) latestSelected.classList.remove("selected")
-                nextSibling.classList.add("selected")
-                latestSelected = nextSibling
+                if (e.shiftKey) {
+                    document.querySelectorAll(".selected").forEach(element => element.classList.remove("selected"))
+                    let start = false
+                    for (const sibling of latestSelected.parentNode.children) {
+                        if (start || sibling === nextSibling || sibling === latestSelected) sibling.classList.add("selected")
+                        if (sibling === latestSelected) start = true
+                        if (sibling === nextSibling) break;
+                    }
+                    latestShiftSelected = nextSibling
+                } else {
+                    latestSelected.classList.remove("selected")
+                    latestSelected = nextSibling
+                    nextSibling.classList.add("selected")
+                }
             }
         }
         else if (e.key === "ArrowLeft") {
+            if (Array.from(latestSelected.parentNode.children).indexOf(latestShiftSelected) > Array.from(latestSelected.parentNode.children).indexOf(latestSelected)) latestShiftSelected = latestSelected
             if (!document.contains(latestSelected)) { selectFirstFile(); return; }
             e.preventDefault()
-            let previousSibling = latestSelected.previousSibling;
+            let previousSibling = e.shiftKey ? latestShiftSelected.previousSibling : latestSelected.previousSibling;
             if (hideHiddenFiles) {
                 while (previousSibling && previousSibling.dataset.hiddenFile !== undefined) {
                     previousSibling = previousSibling.previousSibling
                 }
             }
             if (previousSibling?.className.split(' ').some(function (c) { return /file/.test(c); })) {
-                if (!e.shiftKey) latestSelected.classList.remove("selected")
-                previousSibling.classList.add("selected")
-                latestSelected = previousSibling
+                let start = false
+                if (e.shiftKey) {
+                    document.querySelectorAll(".selected").forEach(element => element.classList.remove("selected"))
+                    for (const sibling of latestSelected.parentNode.children) {
+                        if (start || sibling === previousSibling || sibling === latestSelected) sibling.classList.add("selected")
+                        if (sibling === previousSibling) start = true
+                        if (sibling === latestSelected) break;
+                    }
+                    latestShiftSelected = previousSibling
+                }
+                else {
+                    latestSelected.classList.remove("selected")
+                    latestSelected = previousSibling
+                    previousSibling.classList.add("selected")
+                }
             }
         }
         else if (e.key === "ArrowDown") {
