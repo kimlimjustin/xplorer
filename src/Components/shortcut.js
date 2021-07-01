bd684f1f9b49a531802d59faed85c43479ee117b
kimlimjustin <kimlimjustin@gmail.com>
2021-07-01 22:50:15 +0700

    feat: add open file shortcut

--- src/Components/shortcut.js
+++ src/Components/shortcut.js
@@ -1,4 +1,4 @@
-const copyLocation = require("../Functions/Files/location")
+const copyLocation = require("../Functions/Files/location");
 const { getSelected } = require("../Functions/Files/select")
 
 let selectedAll = true;
@@ -22,12 +22,24 @@ const changeSelectedStatus = () => {
  */
 const Shortcut = () => {
    document.addEventListener("keydown", e => {
+      const selectedFilePath = unescape(getSelected()?.[0]?.dataset?.path)
+      const isDir = getSelected()?.[0]?.dataset.isdir === "true"
+      // Select all shortcut (Ctrl + A)
       if (e.key === "a" && e.ctrlKey) {
          selectedAll = !selectedAll
          if (selectedAll) {
             document.querySelectorAll(".file").forEach(element => element.classList.add("selected"))
          } else document.querySelectorAll(".file").forEach(element => element.classList.remove("selected"))
       }
+      if (e.key === "Enter" && selectedFilePath) {
+         const { openDir, openFileWithDefaultApp } = require("../Functions/Files/open");
+         if (isDir) {
+            openDir(selectedFilePath)
+         } else {
+            openFileWithDefaultApp(selectedFilePath)
+         }
+      }
+      // Copy location path (Alt + Shift + C)
       if (e.key === "C" && e.altKey && e.shiftKey) {
          copyLocation(getSelected()[0])
       }
