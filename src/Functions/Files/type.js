b4e397a1c82308c7636aeed1f204b8279b5d73de
kimlimjustin <kimlimjustin@gmail.com>
2021-07-13 19:58:19 +0700

    refactor: show basename + "file" as type of file as default

--- src/Functions/Files/type.js
+++ src/Functions/Files/type.js
@@ -1,4 +1,5 @@
 const Types = require("../../config/type.json")
+const path = require("path");
 /**
  * Get type of a file name
  * @param {string} filename - File name
@@ -7,9 +8,9 @@ const Types = require("../../config/type.json")
 const getType = (filename) => {
     const ext = filename.split('.').pop().toLowerCase()
     for (const type of Types) {
-        if(type.extension.indexOf(ext) !== -1) return type.type
+        if (type.extension.indexOf(ext) !== -1) return type.type
     }
-    return `${ext.toUpperCase()} file`
+    return `${path.basename(ext).toUpperCase()} file`
 }
 
 module.exports = getType
