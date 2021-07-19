e229f01ca148c6a9b3ecfa62e7bc17f3fdaedd67
kimlimjustin <kimlimjustin@gmail.com>
2021-07-19 20:10:41 +0700

    feat: restore file on windows

--- src/Functions/Files/trash.js
+++ src/Functions/Files/trash.js
@@ -7,6 +7,8 @@ const LINUX_TRASH_FILES_PATH = path.join(os.homedir(), '.local/share/Trash/files
 const LINUX_TRASH_INFO_PATH = path.join(os.homedir(), '.local/share/Trash/info');
 const WINDOWS_TRASH_FILES_PATH = "C:\\Trash/files";
 const WINDOWS_TRASH_INFO_PATH = "C:\\Trash/info";
+const FILES_PATH = process.platform === "win32" ? WINDOWS_TRASH_FILES_PATH : LINUX_TRASH_FILES_PATH
+const INFO_PATH = process.platform === "win32" ? WINDOWS_TRASH_INFO_PATH : LINUX_TRASH_INFO_PATH
 const uuid = require("uuid");
 
 /**
@@ -17,21 +19,21 @@ const uuid = require("uuid");
 const Restore = (filePath) => {
     let fileInfo;
     let __uuid;
-    if (fs.existsSync(path.join(LINUX_TRASH_INFO_PATH, path.basename(filePath) + '.trashinfo'))) {
-        fileInfo = fs.readFileSync(path.join(LINUX_TRASH_INFO_PATH, path.basename(filePath) + '.trashinfo'), "utf8")
+    if (fs.existsSync(path.join(INFO_PATH, path.basename(filePath) + '.trashinfo'))) {
+        fileInfo = fs.readFileSync(path.join(INFO_PATH, path.basename(filePath) + '.trashinfo'), "utf8")
     } else {
-        fs.readdirSync(LINUX_TRASH_INFO_PATH).forEach(info => {
-            if (unescape(fs.readFileSync(path.join(LINUX_TRASH_INFO_PATH, info), "utf-8").split("\n")[1].split("=")[1]) === filePath) {
-                fileInfo = fs.readFileSync(path.join(LINUX_TRASH_INFO_PATH, info), "utf-8")
+        fs.readdirSync(INFO_PATH).forEach(info => {
+            if (unescape(fs.readFileSync(path.join(INFO_PATH, info), "utf-8").split("\n")[1].split("=")[1]) === filePath) {
+                fileInfo = fs.readFileSync(path.join(INFO_PATH, info), "utf-8")
                 __uuid = info.split(".").splice(0, info.split(".").length - 1).join(".");
             }
         })
     }
     const trashSourcePath = fileInfo.split("\n")[1].split("=")[1]
-    fs.rename(path.join(LINUX_TRASH_FILES_PATH, __uuid ?? path.basename(filePath)), unescape(trashSourcePath), (err) => {
+    fs.rename(path.join(FILES_PATH, __uuid ?? path.basename(filePath)), unescape(trashSourcePath), (err) => {
         if (err) ErrorLog(err)
     })
-    fs.unlink(path.join(LINUX_TRASH_INFO_PATH, __uuid ? __uuid + '.trashinfo' : path.basename(filePath) + '.trashinfo'), (err) => {
+    fs.unlink(path.join(INFO_PATH, __uuid ? __uuid + '.trashinfo' : path.basename(filePath) + '.trashinfo'), (err) => {
         if (err) ErrorLog(err)
     })
 }
