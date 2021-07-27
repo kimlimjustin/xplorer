a50502733e0f7c7ef11ad4a1f413edd0645bd7f5
kimlimjustin <kimlimjustin@gmail.com>
2021-07-27 20:08:06 +0700

    style: preview splitting page

--- src/Functions/Files/preview.js
+++ src/Functions/Files/preview.js
@@ -9,10 +9,15 @@ const Preview = (filePath) => {
     const previewElement = document.createElement("div")
     previewElement.classList.add("preview")
     if (path.extname(filePath) === ".pdf") {
-        previewElement.innerHTML = `<object data="${filePath}" type="application/pdf" class="preview-object"><embed src="${filePath}" type="application/pdf" /></object>`
+        previewElement.innerHTML = `
+        <span class="preview-path">${filePath}</span>
+        <span class="preview-exit-btn">&times;</span>
+        <object data="${filePath}" type="application/pdf" class="preview-object"><embed src="${filePath}" type="application/pdf" /></object>`
     }
+    document.querySelector(".main-box").scrollTop = "0"
     document.getElementById("workspace").classList.toggle("workspace-split")
     document.querySelector(".main-box").style.overflowY = "hidden";
+    document.querySelector(`[data-path="${escape(filePath)}"]`).scrollIntoView()
     document.querySelector(".main-box").appendChild(previewElement)
 }
 
