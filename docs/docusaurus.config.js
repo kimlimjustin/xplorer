c9cdf1826585f3ad78f1f651dbaa307faaec838e
kimlimjustin <kimlimjustin@gmail.com>
2021-07-30 20:20:19 +0700

    docs: pwa

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -100,6 +100,34 @@ module.exports = {
         id: 'community',
         routeBasePath: 'community'
       }
-    ]
+    ],
+    [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          'appInstalled',
+          'standalone',
+          'queryString',
+        ],
+        pwaHead: [
+          {
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/icon.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'manifest',
+            href: '/manifest.json', // your PWA manifest
+          },
+          {
+            tagName: 'meta',
+            name: 'theme-color',
+            content: 'rgb(37, 194, 160)',
+          },
+        ],
+      }
+    ],
   ],
 };
