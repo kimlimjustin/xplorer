35be1f97fd97ac5f4dfdc168ec30c7b55ad37b8e
kimlimjustin <kimlimjustin@gmail.com>
2021-08-15 19:54:09 +0700

    fix: wrong dir name bug

--- src/Components/multilingual.js
+++ src/Components/multilingual.js
@@ -9,14 +9,14 @@ const storage = require('electron-json-storage-sync');
  */
 const Translate = (source) => {
     // Read available language
-    const files = fs.readdirSync(path.join(__dirname, "../Languages"))
+    const files = fs.readdirSync(path.join(__dirname, "../Locales"))
     let _returned = false; // Temp variable to check if this function returned something inside that loop.
     let lang = storage.get('preference')?.data?.language ? storage.get('preference')?.data?.language : navigator.language;
     for (const file of files) {
         // Check if the inputed lang available
         if (file.indexOf(lang) !== -1) {
             // Read the language file
-            const langSrc = fs.readFileSync(path.join(__dirname, "../Languages/", file))
+            const langSrc = fs.readFileSync(path.join(__dirname, "../Locales/", file))
             // Replace all text
             Object.keys(JSON.parse(langSrc)).forEach(key => {
                 source = source.replace(new RegExp(key, 'g'), JSON.parse(langSrc)[key])
