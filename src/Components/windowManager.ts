e35d2088cd725b9885e1026885d6673c1f215d76
kimlimjustin <kimlimjustin@gmail.com>
2021-08-20 15:59:00 +0700

    types

---
+++ src/Components/windowManager.ts
@@ -0,0 +1,67 @@
+const { openDir } = require('../Functions/Files/open');
+import storage from 'electron-json-storage-sync';
+import { BrowserWindow } from '@electron/remote';
+import createSidebar from './sidebar';
+const { closePreviewFile } = require('../Functions/Files/preview');
+import windowGUID from '../Constants/windowGUID';
+
+/**
+ * Reload the page
+ * @returns {void}
+ */
+const reload = async () => {
+    const tabs = storage.get(`tabs-${windowGUID}`)?.data;
+    await createSidebar();
+    openDir(tabs.tabs[tabs.focus].position);
+    closePreviewFile();
+};
+
+/**
+ * Minimize Xplorer window
+ * @returns {void}
+ */
+const minimize = (): void => {
+    const electronWindow = BrowserWindow.getFocusedWindow();
+    electronWindow.minimize();
+};
+
+/**
+ * Maximize Xplorer window
+ * @returns {void}
+ */
+const maximize = (): void => {
+    const electronWindow = BrowserWindow.getFocusedWindow();
+    !electronWindow.isMaximized()
+        ? electronWindow.maximize()
+        : electronWindow.unmaximize();
+};
+
+/**
+ * Window manager initializer function
+ * @returns {void}
+ */
+const windowManager = (): void => {
+    // Minimize the screen
+    document.querySelector('#minimize').addEventListener('click', minimize);
+    // Maximize the screen
+    document.querySelector('#maximize').addEventListener('click', maximize);
+    // Exit window
+    document.querySelector('#exit').addEventListener('click', () => {
+        const electronWindow = BrowserWindow.getFocusedWindow();
+        electronWindow.close();
+    });
+
+    // Refresh the page
+    document.querySelector('#refresh').addEventListener('click', reload);
+
+    document
+        .querySelector('.path-navigator')
+        .addEventListener(
+            'change',
+            (event: Event & { target: HTMLInputElement }) => {
+                openDir(event.target.value);
+            }
+        );
+};
+
+export { windowManager, reload, minimize, maximize };
