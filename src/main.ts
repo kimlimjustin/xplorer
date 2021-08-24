284228f6be1c04c6466ae127e4068b9872c28367
kimlimjustin <kimlimjustin@gmail.com>
2021-08-24 19:36:29 +0700

    use electron-builder for building xplorer

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import storage from 'electron-json-storage-sync';
 import * as remoteInit from '@electron/remote/main';
 import isDev from 'electron-is-dev';
 if (isDev) {
-    try {
-        const electronReloader = require('electron-reloader');
-        electronReloader(module);
-    } catch (_) {}
+	try {
+		const electronReloader = require('electron-reloader');
+		electronReloader(module);
+	} catch (_) {}
 }
 remoteInit.initialize();
 
@@ -20,66 +20,66 @@ console.log(process.argv);
 
 let id: string;
 ipcMain.on('GUID', (_, arg: string) => {
-    id = arg;
+	id = arg;
 });
 
 // Create a new window
 function createWindow() {
-    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-    const win = new BrowserWindow({
-        title: 'xplorer',
-        frame: false,
-        width: Math.floor(width * 0.8),
-        height: Math.floor(height * 0.8),
-        minWidth: 600,
-        minHeight: 400,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            enableRemoteModule: true,
-        },
-    });
+	const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+	const win = new BrowserWindow({
+		title: 'xplorer',
+		frame: false,
+		width: Math.floor(width * 0.8),
+		height: Math.floor(height * 0.8),
+		minWidth: 600,
+		minHeight: 400,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			enableRemoteModule: true,
+		},
+	});
 
-    win.loadFile('src/public/index.html');
-    win.webContents.on(
-        'new-window',
-        (e: Electron.NewWindowWebContentsEvent, url: string) => {
-            e.preventDefault();
-            shell.openExternal(url);
-        }
-    );
+	win.loadFile(isDev ? './public/index.html' : 'src/public/index.html');
+	win.webContents.on(
+		'new-window',
+		(e: Electron.NewWindowWebContentsEvent, url: string) => {
+			e.preventDefault();
+			shell.openExternal(url);
+		}
+	);
 }
 
 app.allowRendererProcessReuse = false;
 
 app.whenReady().then(() => {
-    createWindow();
+	createWindow();
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-    });
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
 });
 
 app.on('window-all-closed', () => {
-    storage.remove(`tabs-${id}`);
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+	storage.remove(`tabs-${id}`);
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
 interface dragOptions {
-    [key: string]: string;
+	[key: string]: string;
 }
 
 ipcMain.on(
-    'ondragstart',
-    (event: Electron.IpcMainEvent, filePath: string, options: dragOptions) => {
-        event.sender.startDrag({
-            file: filePath,
-            icon: options.isDir
-                ? path.join(__dirname, 'icon/folder.png')
-                : path.join(__dirname, 'icon/file.png'),
-        });
-    }
+	'ondragstart',
+	(event: Electron.IpcMainEvent, filePath: string, options: dragOptions) => {
+		event.sender.startDrag({
+			file: filePath,
+			icon: options.isDir
+				? path.join(__dirname, 'icon/folder.png')
+				: path.join(__dirname, 'icon/file.png'),
+		});
+	}
 );
