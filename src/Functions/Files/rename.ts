f210134512292712179a19ecdcab0faeb54994d6
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2021-09-03 17:32:57 +0700

    fix: rename file bugs
    fix following bug:
    - File will go into root of a drive on Windows if user remove the directory name
    - Special character bug

--- src/Functions/Files/rename.ts
+++ src/Functions/Files/rename.ts
@@ -5,6 +5,8 @@ import { ErrorLog } from '../Logs/log';
 import { dialog } from '@electron/remote';
 import storage from 'electron-json-storage-sync';
 import { detectDefaultTheme } from '../Theme/theme';
+import windowGUID from '../../Constants/windowGUID';
+import os from 'os';
 
 /**
  * Rename file/folder name
@@ -25,27 +27,32 @@ const Rename = (filePath: string): void => {
 		label: 'New Name:',
 		inputAttrs: { type: 'text', required: true },
 		type: 'input',
-		value: path.basename(filePath),
+		value: unescape(path.basename(filePath)),
 		icon: path.join(__dirname, '../../../../icons/icon.png'),
 		alwaysOnTop: true,
 		customStylesheet,
 	}).then((newName: string) => {
-		fs.rename(
-			filePath,
-			path.join(path.dirname(filePath), newName),
-			(err) => {
-				if (err) {
-					dialog.showMessageBoxSync({
-						message:
-							'Something went wrong, please try again or open an issue on GitHub.',
-						type: 'error',
-					});
-					ErrorLog(err);
-				}
+		const tabs = storage.get(`tabs-${windowGUID}`)?.data;
+		const focusingPath =
+			tabs.tabs[tabs.focus].position === 'Home' ||
+			tabs.tabs[tabs.focus].position === path.join(os.homedir(), 'Home')
+				? os.homedir()
+				: tabs.tabs[tabs.focus].position;
+		const target =
+			path.dirname(newName) === '.'
+				? path.join(focusingPath, newName)
+				: path.join(path.dirname(filePath), newName);
+		fs.rename(unescape(filePath), target, (err) => {
+			if (err) {
+				dialog.showMessageBoxSync({
+					message:
+						'Something went wrong, please try again or open an issue on GitHub.',
+					type: 'error',
+				});
+				ErrorLog(err);
 			}
-		);
+		});
 	});
-	console.log(path);
 };
 
 export default Rename;
