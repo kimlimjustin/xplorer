3df25c86f19c770a8be6f237a52aa7870a120c13
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2021-09-30 19:53:02 +0700

    feat(locales): internationalized setting

--- src/Components/Setting/Preference/preference.ts
+++ src/Components/Setting/Preference/preference.ts
@@ -3,6 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import { reload } from '../../Layout/windowManager';
 import ejs from 'ejs';
+import Translate from '../../I18n/i18n';
 /**
  * Create preference section
  * @returns {void}
@@ -26,12 +27,24 @@ const Preference = (): void => {
 		storage.get('preference')?.data?.dirAlongsideFiles ?? false;
 	const settingsMain = document.querySelector('.settings-main');
 
+	const appLanguage_i18n = Translate('App Language');
+	const fileAndFolders_i18n = Translate('Files and Folders');
+	const hideHiddenFiles_i18n = Translate('Hide hidden files');
+	const hideSystemFiles_i18n = Translate('Hide system files');
+	const dirAlongsideFiles_i18n = Translate(
+		'List and sort directories alongside files'
+	);
 	ejs.renderFile(path.join(__dirname, 'preference.ejs'), {
 		language,
 		hideHiddenFiles,
 		hideSystemFiles,
 		dirAlongsideFiles,
 		availableLanguages,
+		appLanguage_i18n,
+		fileAndFolders_i18n,
+		hideHiddenFiles_i18n,
+		hideSystemFiles_i18n,
+		dirAlongsideFiles_i18n,
 	}).then((result) => {
 		settingsMain.innerHTML = result;
 		settingsMain
