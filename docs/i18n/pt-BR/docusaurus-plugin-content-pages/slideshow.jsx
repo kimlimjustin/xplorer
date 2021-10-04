90efe69fa5774c90efbc9f60c412b0910ccb9a34
Anil Seervi <anil13112000@gmail.com>
2021-10-04 17:52:05 +0530

    fix slideshow images not displaying for languages except english.

--- docs/i18n/pt-BR/docusaurus-plugin-content-pages/slideshow.jsx
+++ docs/i18n/pt-BR/docusaurus-plugin-content-pages/slideshow.jsx
@@ -1,61 +1,53 @@
-import React, { useEffect } from 'react';
-export default function Slideshow() {
-	useEffect(() => {
-		var slideIndex = 0;
+import React, { useEffect, useState } from 'react';
 
-		const showSlides = () => {
-			var i;
-			var slides = document.getElementsByClassName('slide');
-			var dots = document.getElementsByClassName('slide-dot');
-			for (i = 0; i < slides.length; i++) {
-				slides[i].style.display = 'none';
-			}
-			slideIndex++;
-			if (slideIndex > slides.length) {
-				slideIndex = 1;
-			}
-			for (i = 0; i < dots.length; i++) {
-				dots[i].className = dots[i].className.replace(
-					' slide-active',
-					''
-				);
-			}
-			slides[slideIndex - 1].style.display = 'block';
-			dots[slideIndex - 1].className += ' slide-active';
-			setTimeout(showSlides, 2000);
-		};
-		showSlides();
-	}, []);
-	return (
-		<>
-			<div className="slideshow-container">
-				<div className="slide">
-					<div className="slide-numbertext">1 / 3</div>
-					<img
-						src="/img/Xplorer%20win.png"
-						alt="Xplorer on Windows"
-					/>
-					<div className="slide-caption">Windows</div>
-				</div>
-				<div className="slide">
-					<div className="slide-numbertext">2 / 3</div>
-					<img
-						src="/img/Xplorer%20linux.png"
-						alt="Xplorer on Linux"
-					/>
-					<div className="slide-caption">Garuda Linux</div>
-				</div>
-				<div className="slide">
-					<div className="slide-numbertext">3 / 3</div>
-					<img src="/img/Xplorer%20mac.png" alt="Xplorer on macOS" />
-					<div className="slide-caption">macOS Catalina</div>
-				</div>
-			</div>
-			<div className="slide-dots">
-				<span className="slide-dot"></span>
-				<span className="slide-dot"></span>
-				<span className="slide-dot"></span>
-			</div>
-		</>
-	);
+const slides = [
+    {
+        name: 'Windows',
+        src: '/img/Xplorer%20win.png',
+        alt: 'Xplorer on Windows',
+    },
+    {
+        name: 'Garuda Linux',
+        src: '/img/Xplorer%20linux.png',
+        alt: 'Xplorer on Linux',
+    },
+    {
+        name: 'macOS Catalina',
+        src: '/img/Xplorer%20mac.png',
+        alt: 'Xplorer on macOS',
+    },
+];
+
+export default function Slideshow() {
+    const [index, setIndex] = useState(0);
+    useEffect(() => {
+        const handle = setInterval(() => setIndex((i) => (i + 1) % 3), 2500);
+        return () => clearInterval(handle);
+    }, []);
+    return (
+        <>
+            <div className="slideshow-container">
+                {slides.map((e, i) => (
+                    <div
+                        key={i}
+                        className={`slide${index === i ? ' active' : ''}`}
+                    >
+                        <div className="slide-numbertext">
+                            {i + 1} / {slides.length}
+                        </div>
+                        <img src={e.src} alt={e.alt} />
+                        <div className="slide-caption">{e.name}</div>
+                    </div>
+                ))}
+            </div>
+            <div className="slide-dots">
+                {[0, 1, 2].map((e) => (
+                    <span
+                        key={e}
+                        className={`slide-dot${index === e ? ' active' : ''}`}
+                    />
+                ))}
+            </div>
+        </>
+    );
 }
