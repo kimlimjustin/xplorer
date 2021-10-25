7a42ac4994e171a6ef6a8a61cc5b1f940dd9f292
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2021-10-25 22:28:38 +0700

    feat: files section on home page for unix

---
+++ src/Components/Functions/displayFiles.ts
@@ -0,0 +1,162 @@
+import { OpenLog } from './log';
+import Storage from '../../Api/storage';
+import fileThumbnail from '../Thumbnail/thumbnail';
+import formatBytes from './filesize';
+import getType from '../Files/File Type/type';
+import type { FileMetaData } from '../../Api/directory';
+/**
+ * Display files into Xplorer main section
+ * @param {fileData[]} files - array of files of a directory
+ * @param {string} dir - directory base path
+ * @param {HTMLElement} onElement - element to append files element
+ * @returns {Promise<HTMLElement>}
+ */
+const displayFiles = async (
+	files: FileMetaData[],
+	dir: string,
+	onElement?: HTMLElement
+	//options?: { reveal: boolean; initialDirToOpen: string }
+): Promise<HTMLElement> => {
+	const FilesElement = onElement ?? document.createElement('div');
+	const preference = await Storage.get('preference');
+	const hideSystemFile = preference?.hideSystemFiles ?? true;
+	const dirAlongsideFiles = preference?.dirAlongsideFiles ?? false;
+	const layout =
+		(await Storage.get('layout'))?.[dir] ?? preference?.layout ?? 's';
+	const sort = (await Storage.get('sort'))?.[dir] ?? 'A';
+
+	files = files.sort((a, b) => {
+		switch (sort) {
+			case 'A': // A-Z
+				return a.basename.toLowerCase() > b.basename.toLowerCase()
+					? 1
+					: -1;
+			case 'Z': // Z-A
+				return a.basename.toLowerCase() < b.basename.toLowerCase()
+					? 1
+					: -1;
+			case 'L': // Last Modified
+				return new Date(a.last_modified.secs_since_epoch) <
+					new Date(b.last_modified.secs_since_epoch)
+					? 1
+					: -1;
+			case 'F': // First Modified
+				return new Date(a.last_modified.secs_since_epoch) >
+					new Date(b.last_modified.secs_since_epoch)
+					? 1
+					: -1;
+			case 'S': // Size
+				return a.size > b.size ? 1 : -1;
+			case 'T':
+				return 1;
+			//return getType(a.basename) > getType(b.basename) ? 1 : -1;
+		}
+	});
+	if (!dirAlongsideFiles) {
+		files = files.sort((a, b) => -(Number(a.is_dir) - Number(b.is_dir)));
+	}
+	if (hideSystemFile) {
+		files = files.filter((file) => !file.is_system);
+	}
+
+	for (const file of files) {
+		const fileType = getType(file.basename, file.is_dir);
+		const preview = await fileThumbnail(
+			file.file_path,
+			file.is_dir ? 'folder' : 'file'
+		);
+		const fileGrid = document.createElement('div');
+		fileGrid.className = 'file-grid grid-hover-effect file';
+		//if (dirent.isTrash) fileGrid.dataset.isTrash = 'true';
+		let displayName: string;
+		switch (layout) {
+			case 'm':
+				fileGrid.classList.add('medium-grid-view');
+				displayName =
+					file.basename.length > 30
+						? file.basename.substring(0, 30) + '...'
+						: file.basename;
+				break;
+			case 'l':
+				fileGrid.classList.add('large-grid-view');
+				displayName =
+					file.basename.length > 40
+						? file.basename.substring(0, 40) + '...'
+						: file.basename;
+				break;
+			case 'd':
+				fileGrid.classList.add('detail-view');
+				displayName = file.basename;
+				break;
+			default:
+				fileGrid.classList.add('small-grid-view');
+				displayName =
+					file.basename.length > 20
+						? file.basename.substring(0, 20) + '...'
+						: file.basename;
+				break;
+		}
+		fileGrid.setAttribute('draggable', 'true');
+		fileGrid.dataset.modifiedAt = String(
+			new Date(file.last_modified.secs_since_epoch * 1000).toLocaleString(
+				navigator.language,
+				{ hour12: false }
+			)
+		);
+		fileGrid.dataset.createdAt = String(
+			new Date(file.created.secs_since_epoch * 1000).toLocaleString(
+				navigator.language,
+				{ hour12: false }
+			)
+		);
+		fileGrid.dataset.accessedAt = String(
+			new Date(file.last_accessed.secs_since_epoch * 1000).toLocaleString(
+				navigator.language,
+				{ hour12: false }
+			)
+		);
+		fileGrid.dataset.isdir = String(file.is_dir);
+		/*if (dirent.trashDeletionDate)
+                fileGrid.dataset.trashDeletionDate = String(
+                    dirent.trashDeletionDate
+                );*/
+		if (file.is_hidden) fileGrid.dataset.hiddenFile = 'true';
+		/*if (dirent.realPath)
+                fileGrid.dataset.realPath = escape(
+                    dirent.realPath ?? path.join(dir, dirent.name)
+                );*/
+		fileGrid.dataset.path = escape(file.file_path);
+		fileGrid.innerHTML = `
+            ${preview}
+            <span class="file-grid-filename" id="file-filename">${displayName}</span><span class="file-modifiedAt" id="file-timestamp">${new Date(
+			file.last_modified.secs_since_epoch * 1000
+		).toLocaleString(navigator.language, { hour12: false })}</span>
+            ${
+				file.size > 0
+					? `<span class="file-size" id="file-size">${formatBytes(
+							file.size // eslint-disable-next-line no-mixed-spaces-and-tabs
+					  )}</span>`
+					: `<span class="file-size" id="file-size"></span>`
+			}
+            <span class="file-type">${fileType}</span>
+            `;
+		FilesElement.appendChild(fileGrid);
+	}
+	/*if (options?.reveal || !fs.statSync(dir)?.isDirectory()) {
+		Select(
+			document.querySelector<HTMLElement>(
+				`[data-path="${escape(options?.initialDirToOpen)}"]`
+				),
+				false,
+				false,
+				document.querySelectorAll('.file')
+				);
+			}*/
+
+	/*SelectListener(document.querySelectorAll('.file'));*/
+
+	OpenLog(dir);
+	return FilesElement;
+};
+
+export default displayFiles;
--- src/Components/Layout/home.ts
+++ src/Components/Layout/home.ts
@@ -1,173 +1,18 @@
 import { Drives } from '../Drives/drives';
 import Favorites from '../Favorites/favorites';
 import { updateTheme } from '../Theme/theme';
-/*import Translate from '../I18n/i18n';
-import nativeDrag from '../Files/File Operation/drag';
-import fileThumbnail from '../Thumbnail/thumbnail';*/
 import { startLoading, stopLoading } from '../Functions/Loading/loading';
-/*import LAZY_LOAD from '../Functions/lazyLoadingImage';
-import { isHiddenFile } from 'is-hidden-file';
-import getType from '../Files/File Type/type';
-import formatBytes from '../Functions/filesize';
-import windowGUID from '../Constants/windowGUID';
-import type fileData from '../../Typings/fileData';
-import { ErrorLog } from '../Functions/log';*/
 import OS from '../../Api/platform';
+import LAZY_LOAD from '../Functions/lazyLoadingImage';
+import DirectoryAPI from '../../Api/directory';
+import FavoritesAPI from '../../Api/favorites';
+import displayFiles from '../Functions/displayFiles';
 let platform: string;
 (async () => {
 	platform = await OS();
 })();
 
-/**
- * Create home files section (only for linux)
- * @param {any} callback
- * @returns {any} home file section
- */
-/*type cb = (res: string) => void;
-const homeFiles = (callback: cb) => {
-	const readHomeFiles = async () => {
-		const dirAlongsideFiles =
-			JSON.parse(localStorage?.getItem('preference'))
-				?.dirAlongsideFiles ?? false;
-		const layout =
-			JSON.parse(localStorage.getItem('layout'))?.[os.homedir()] ??
-			JSON.parse(localStorage.getItem('preference'))?.layout ??
-			's';
-		const sort =
-			JSON.parse(localStorage.getItem('sort'))?.[os.homedir()] ?? 'A';
-		let result = `<section class='home-section'><h1 class="section-title">Files</h1>`;
-		let files = fs
-			.readdirSync(os.homedir(), { withFileTypes: true })
-			.map((dirent) => {
-				try {
-					const result: fileData = {
-						name: dirent.name,
-						isDir: dirent.isDirectory(),
-						isHidden: isHiddenFile(
-							path.join(os.homedir(), dirent.name)
-						),
-						displayName: dirent.name,
-					};
-					const type = getType(
-						path.join(os.homedir(), dirent.name),
-						dirent.isDirectory()
-					);
-					result.type = type;
-					const stat = fs.statSync(
-						path.join(os.homedir(), dirent.name)
-					);
-					result.createdAt = stat.ctime;
-					result.modifiedAt = stat.mtime;
-					result.accessedAt = stat.atime;
-					result.size = stat.size;
-					return result;
-				} catch (_) {
-					ErrorLog(`Error reading ${dirent.name}`);
-				}
-			});
-		files = files.sort((a, b) => {
-			switch (sort) {
-				case 'A': // A-Z
-					return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1;
-				case 'Z': // Z-A
-					return a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1;
-				case 'L': // Last Modified
-					return new Date(a.modifiedAt) < new Date(b.modifiedAt)
-						? 1
-						: -1;
-				case 'F': // First Modified
-					return new Date(a.modifiedAt) > new Date(b.modifiedAt)
-						? 1
-						: -1;
-				case 'S': // Size
-					return a.size > b.size ? 1 : -1;
-				case 'T':
-					return a.type > b.type ? 1 : -1;
-			}
-		});
-		if (!dirAlongsideFiles) {
-			files = files.sort((a, b) => -(Number(a.isDir) - Number(b.isDir)));
-		}
-		await files.forEach(async (file) => {
-			const preview = await fileThumbnail(
-				path.join(os.homedir(), file.name),
-				file.isDir ? 'folder' : 'file'
-			);
-			let className = 'file file-grid grid-hover-effect';
-			switch (layout) {
-				case 'm':
-					className += ' medium-grid-view';
-					file.displayName =
-						file.name.length > 30
-							? file.name.substring(0, 30) + '...'
-							: file.name;
-					break;
-				case 'l':
-					className += ' large-grid-view';
-					file.displayName =
-						file.name.length > 40
-							? file.name.substring(0, 40) + '...'
-							: file.name;
-					break;
-				case 'd':
-					className += ' detail-view';
-					break;
-				default:
-					className += ' small-grid-view';
-					file.displayName =
-						file.name.length > 20
-							? file.name.substring(0, 20) + '...'
-							: file.name;
-					break;
-			}
-			result += `<div class="${className}" draggable="true" data-isdir=${
-				file.isDir
-			} data-path = "${escape(path.join(os.homedir(), file.name))}" ${
-				isHiddenFile(path.join(os.homedir(), file.name))
-					? 'data-hidden-file'
-					: ''
-			} data-size="${file.size}" data-created-at="${
-				file.createdAt
-			}" data-modified-at="${file.modifiedAt}" data-accessed-at="${
-				file.accessedAt
-			}">
-            ${preview}
-            <span class="file-grid-filename" id="file-filename">${Translate(
-				file.displayName
-			)}</span><span class="file-modifiedAt" id="file-createdAt">${new Date(
-				file.modifiedAt
-			).toLocaleString(navigator.language, { hour12: false })}</span>
-            ${
-				file.size > 0
-					? `<span class="file-size" id="file-size">${formatBytes(
-							file.size
-					  )}</span>` //eslint-disable-line
-					: `<span class="file-size" id="file-size"></span>` //eslint-disable-line
-			}
-            </div>`;
-		});
-		callback(result + '</section>');
-	};
-	readHomeFiles();
-
-	// Watch the directory
-	const watcher = fs.watch(os.homedir(), async () => {
-		readHomeFiles();
-	});
-
-	let focusingPath: string; // Watch if focusing path changes
-	setInterval(() => {
-		const tabs = storage.get(`tabs-${windowGUID}`)?.data;
-		const _focusingPath = tabs.tabs[tabs.focus]?.position;
-		if (focusingPath === undefined) {
-			focusingPath = _focusingPath;
-		} else {
-			if (focusingPath !== _focusingPath) {
-				watcher.close();
-			}
-		}
-	}, 500);
-};*/
+let favoritesData: FavoritesAPI;
 
 /**
  * Create contents for home page
@@ -181,24 +26,36 @@ const Home = async (): Promise<void> => {
 		MAIN_ELEMENT.classList.remove('empty-dir-notification'); // Remove class if exist
 	const favorites = await Favorites();
 	const drives = await Drives();
-	/*if (platform !== 'win32') {
-		homeFiles((files) => {
-			// Update the content in the main page ...
-			MAIN_ELEMENT.innerHTML = favorites + drives + files;
-
-			// And also the theme :)
-			updateTheme();
-			nativeDrag(document.querySelectorAll('.file'), os.homedir()); // Listen to native drag
-			stopLoading();
-			LAZY_LOAD();
-		});
-	} else {*/
-	// Update the content in the main page ...
 	MAIN_ELEMENT.innerHTML = favorites + drives;
+	if (platform !== 'win32') {
+		if (!favoritesData?.HOMEDIR_PATH) {
+			favoritesData = await new FavoritesAPI();
+			await favoritesData.build();
+		}
+		const directoryInfo = new DirectoryAPI(favoritesData.HOMEDIR_PATH);
+		const homeElement = document.createElement('section');
+		homeElement.classList.add('home-section');
+		homeElement.innerHTML = '<h1 class="section-title">Files</section>';
+
+		const homeFiles = await directoryInfo.getFiles();
+		const homeSection = await displayFiles(
+			homeFiles.files,
+			favoritesData.HOMEDIR_PATH,
+			homeElement
+		);
+		// Update the content in the main page ...
+		MAIN_ELEMENT.innerHTML = favorites + drives;
+		MAIN_ELEMENT.appendChild(homeSection);
+
+		// And also the theme :)
+		updateTheme();
+		stopLoading();
+		LAZY_LOAD();
+	}
+	// Update the content in the main page ...
 	// And also the theme :)
 	updateTheme();
 	stopLoading();
-	//}
 };
 
 export default Home;
