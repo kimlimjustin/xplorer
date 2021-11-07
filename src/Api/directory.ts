846d7e3317c5498ce10470793432ca72bb4d2f21
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2021-11-07 20:31:45 +0700

    feat: reimplement dir listener

--- src/Api/directory.ts
+++ src/Api/directory.ts
@@ -2,7 +2,8 @@ import joinPath from '../Components/Functions/path/joinPath';
 import normalizeSlash from '../Components/Functions/path/normalizeSlash';
 import { invoke } from '@tauri-apps/api';
 import type FileMetaData from '../Typings/fileMetaData';
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let interval: any;
 interface DirectoryData {
 	files: FileMetaData[];
 	number_of_files: number;
@@ -14,6 +15,7 @@ interface DirectoryData {
 class DirectoryAPI {
 	readonly dirName: string;
 	readonly parentDir: string;
+	private _files: string[] = [];
 	files: FileMetaData[];
 	constructor(dirName: string, parentDir?: string) {
 		if (parentDir) {
@@ -27,12 +29,10 @@ class DirectoryAPI {
 	 */
 	getFiles(): Promise<DirectoryData> {
 		return new Promise((resolve) => {
-			invoke('read_directory', { dir: this.dirName }).then(
-				(files: DirectoryData) => {
-					this.files = files.files;
-					resolve(files);
-				}
-			);
+			invoke('read_directory', { dir: this.dirName }).then((files: DirectoryData) => {
+				this.files = files.files;
+				resolve(files);
+			});
 		});
 	}
 	/**
@@ -41,9 +41,7 @@ class DirectoryAPI {
 	 */
 	async isDir(): Promise<boolean> {
 		return new Promise((resolve) => {
-			invoke('is_dir', { path: this.dirName }).then((result: boolean) =>
-				resolve(result)
-			);
+			invoke('is_dir', { path: this.dirName }).then((result: boolean) => resolve(result));
 		});
 	}
 	/**
@@ -63,8 +61,25 @@ class DirectoryAPI {
 		});
 	}
 
-	listen(): void {
-		null;
+	/**
+	 * Listen to changes in a directory
+	 * @param {() => void} cb - callback
+	 * @returns {any}
+	 */
+	async listen(cb: () => void): Promise<void> {
+		clearInterval(interval);
+		this._files = await invoke('get_files_in_directory', { dir: this.dirName });
+		interval = setInterval(
+			async () => {
+				const files = (await invoke('get_files_in_directory', { dir: this.dirName })) as string[];
+				if (JSON.stringify(files) !== JSON.stringify(this._files)) {
+					console.log('a');
+					cb();
+				}
+				this._files = files;
+			},
+			this._files.length > 200 ? this._files.length * 5 : 500
+		);
 	}
 }
 
--- src/Components/Open/open.ts
+++ src/Components/Open/open.ts
@@ -14,6 +14,7 @@ import { changeWindowTitle } from '../../Api/window';
 import getBasename from '../Functions/path/basename';
 import { getTrashedFiles } from '../../Api/trash';
 import OS from '../../Api/platform';
+import { reload } from '../Layout/windowManager';
 let platform: string;
 /**
  * Open a directory on Xplorer
@@ -26,8 +27,7 @@ const OpenDir = async (dir: string, reveal?: boolean): Promise<void> => {
 	changePosition(dir);
 	const MAIN_ELEMENT = document.getElementById('workspace');
 	MAIN_ELEMENT.innerHTML = '';
-	if (MAIN_ELEMENT.classList.contains('empty-dir-notification'))
-		MAIN_ELEMENT.classList.remove('empty-dir-notification'); // Remove class if exist
+	if (MAIN_ELEMENT.classList.contains('empty-dir-notification')) MAIN_ELEMENT.classList.remove('empty-dir-notification'); // Remove class if exist
 
 	if (dir === 'xplorer://Home') {
 		Home();
@@ -35,8 +35,7 @@ const OpenDir = async (dir: string, reveal?: boolean): Promise<void> => {
 		if (!platform) platform = await OS();
 		if (platform === 'darwin') {
 			MAIN_ELEMENT.classList.add('empty-dir-notification');
-			MAIN_ELEMENT.innerText =
-				'Xploring trash folder is not supported for macOS yet.';
+			MAIN_ELEMENT.innerText = 'Xploring trash folder is not supported for macOS yet.';
 			stopLoading();
 		} else {
 			getTrashedFiles().then(async (trashedFiles) => {
@@ -87,6 +86,7 @@ const OpenDir = async (dir: string, reveal?: boolean): Promise<void> => {
 					updateTheme();
 					LAZY_LOAD();
 					changeWindowTitle(getBasename(dir));
+					directoryInfo.listen(() => reload());
 				}
 			});
 		}
@@ -119,163 +119,7 @@ const OpenHandler = (e: Event): void => {
  * @returns {void}
  */
 const OpenInit = (): void => {
-	document
-		.querySelector('#sidebar-nav')
-		.addEventListener('click', OpenHandler);
-	document
-		.querySelector('#workspace')
-		.addEventListener('dblclick', OpenHandler);
+	document.querySelector('#sidebar-nav').addEventListener('click', OpenHandler);
+	document.querySelector('#workspace').addEventListener('dblclick', OpenHandler);
 };
-/*import fileThumbnail from "../../Thumbnail/thumbnail";
-import Home from '../../Layout/home';
-import changePosition from "../../Functions/changePosition";
-import { updateTheme } from "../../Theme/theme";
-import nativeDrag from "./drag";
-import { startLoading, stopLoading } from "../../Functions/Loading/loading";
-import Recent from "../../Recent/recent";
-import LAZY_LOAD from "../../Functions/lazyLoadingImage";
-import formatBytes from "../../Functions/filesize";
-import getType from "../File Type/type";
-import { SelectListener, Select } from "./select";
-import { InfoLog, ErrorLog } from "../../Functions/log";
-import { closePreviewFile } from "../File Preview/preview";
-import {dialog} from "@electron/remote";
-import type fileData from "../../../Typings/fileData";
-import { ipcRenderer } from "electron";
-import { FSWatcher } from "original-fs";
-
-const WINDOWS_TRASH_FILES_PATH = "C:\\Trash/files";
-const WINDOWS_TRASH_INFO_PATH = "C:\\Trash/info";
-const LINUX_TRASH_FILES_PATH = path.join(os.homedir(), '.local/share/Trash/files')
-const LINUX_TRASH_INFO_PATH = path.join(os.homedir(), '.local/share/Trash/info')
-const IGNORE_FILE = ['.', '..'];
-
-let timeStarted:number;
-let watcher:undefined|FSWatcher;
-
-*/
-
-/**
- * Open a directory on Xplorer
- * @param {string} dir
- * @param {boolean} boolean - Open the parent directory and select the file/dir
- * @returns {void}
- */
-/*const open = (dir:string, reveal?:boolean):void => {
-    if (!dir) return
-
-    const initialDirToOpen = dir;
-    closePreviewFile()
-    timeStarted = Date.now()
-    startLoading()
-    changePosition(dir)
-    if (dir === "xplorer://Home") {
-        Home(() => {
-            SelectListener(document.querySelectorAll(".file"))
-            InfoLog(`Open ${dir} within ${(Date.now() - timeStarted) / 1000}s`)
-            console.log(`Open ${dir} within ${(Date.now() - timeStarted) / 1000}s`)
-        })
-    } else if ( dir === "xplorer://Recent") {
-        Recent()
-    } else if ( dir === "xplorer://Trash") {
-        const getFiles = () => {
-            if (process.platform === "win32") {
-                if (!fs.existsSync(WINDOWS_TRASH_FILES_PATH)) return []
-                else {
-                    return fs.readdirSync(WINDOWS_TRASH_FILES_PATH, { withFileTypes: true }).map(dirent => {
-                        const fileInfo = fs.readFileSync(path.join(WINDOWS_TRASH_INFO_PATH, dirent.name + '.trashinfo'), 'utf8').split("\n")
-                        let trashPath, trashDeletionDate;
-                        if (fileInfo[0] === "[Trash Info]") {
-                            trashPath = fileInfo[1].split('=')[1]
-                            trashDeletionDate = fileInfo[2].split("=")[1]
-                        }
-                        const type = getType(unescape(trashPath) ?? path.join(dir, dirent.name), dirent.isDirectory() )
-                        return { name: unescape(trashPath), isDir: dirent.isDirectory(), isHidden: isHiddenFile(path.join(dir, dirent.name)), trashPath, trashDeletionDate, type, isTrash: true, path: unescape(trashPath), realPath: path.join(WINDOWS_TRASH_FILES_PATH, dirent.name) };
-                    })
-                }
-            } else {
-                if (!fs.existsSync(LINUX_TRASH_FILES_PATH)) return []
-                else {
-                    return fs.readdirSync(LINUX_TRASH_FILES_PATH, { withFileTypes: true }).map(dirent => {
-                        const fileInfo = fs.readFileSync(path.join(LINUX_TRASH_INFO_PATH, dirent.name + '.trashinfo'), 'utf8').split("\n")
-                        let trashPath, trashDeletionDate;
-                        if (fileInfo[0] === "[Trash Info]") {
-                            trashPath = fileInfo[1].split('=')[1]
-                            trashDeletionDate = fileInfo[2].split("=")[1]
-                        }
-                        const type = getType(unescape(trashPath) ?? path.join(dir, dirent.name), dirent.isDirectory() )
-
-                        return { name: unescape(trashPath), isDir: dirent.isDirectory(), isHidden: isHiddenFile(path.join(dir, dirent.name)), trashPath, trashDeletionDate, type, isTrash: true, path: unescape(trashPath), realPath: path.join(LINUX_TRASH_FILES_PATH, dirent.name) };
-                    })
-                }
-            }
-        }
-        const files = getFiles()
-        displayFiles(files, process.platform === "win32" ? WINDOWS_TRASH_FILES_PATH : LINUX_TRASH_FILES_PATH)
-        // Watch the directory
-        watcher?.close()
-        fs.watch(process.platform === "win32" ? WINDOWS_TRASH_FILES_PATH : LINUX_TRASH_FILES_PATH, async () => {
-            const files = getFiles()
-            // Get files of the dir
-            displayFiles(files, process.platform === "win32" ? WINDOWS_TRASH_FILES_PATH : LINUX_TRASH_FILES_PATH)
-        })
-
-    } else {
-        if (!fs.existsSync(dir)) {
-            ErrorLog(`${dir} does not exist.`)
-            stopLoading()
-            if(dialog.showMessageBoxSync({ message: `'${dir}' does not exist. do you want to create it?.`, type: "error", buttons: ["Yes", "No"] }) === 0){
-                fs.mkdir(dir, (err) => {
-                    if(err) dialog.showMessageBoxSync({message: 'Error creating folder. Please try again', type: "error"})
-                })
-            } else return;
-        }
-        if(reveal || !fs.statSync(dir)?.isDirectory()){
-            dir = path.dirname(dir)
-        }
-        const hideSystemFile = storage.get("preference")?.data?.hideSystemFiles ?? true
-        let getAttributesSync:any; //eslint-disable-line
-        if (process.platform === "win32") getAttributesSync = require("fswin").getAttributesSync; //eslint-disable-line
-        const getFiles = () => {
-            return fs.readdirSync(dir, { withFileTypes: true }).map(dirent => {
-                const result:fileData = { name: dirent.name, isDir: dirent.isDirectory(), isHidden: isHiddenFile(path.join(dir, dirent.name)) }
-                const type = getType(path.join(dir, dirent.name), dirent.isDirectory())
-                result.type = type
-                try {
-                    const stat = fs.statSync(path.join(dir, dirent.name))
-                    result.createdAt = stat.ctime
-                    result.modifiedAt = stat.mtime
-                    result.accessedAt = stat.atime
-                    result.size = stat.size
-                } catch (_) {
-                    if (process.platform === "win32" && !hideSystemFile) {
-                        const stat = getAttributesSync(path.join(dir, dirent.name));
-                        if (stat) {
-                            result.createdAt = stat.CREATION_TIME;
-                            result.modifiedAt = stat.LAST_WRITE_TIME;
-                            result.accessedAt = stat.LAST_ACCESS_TIME;
-                            result.size = stat.SIZE;
-                        }
-                    }
-                    result.isSystemFile = true
-                }
-                return result
-            })
-        }
-        const files = getFiles()
-        displayFiles(files, dir, {reveal, initialDirToOpen})
-        // Watch the directory
-        watcher?.close()
-        watcher = fs.watch(dir, async (_, filePath) => {
-            // Check if the file is under operation on Xplorer
-            if(!ipcRenderer.sendSync('under-operation', path.join(dir, filePath))){
-                const files = getFiles()
-                // Get files of the dir
-                displayFiles(files, dir)
-            }
-        })
-    }
-}
-   
-export { open, openFileWithDefaultApp, closeWatcher }*/
 export { OpenInit, OpenDir };
