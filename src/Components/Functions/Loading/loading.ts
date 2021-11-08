581b5e270fe4244d8ab1d9d197eb786cff0a9d44
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2021-11-08 20:54:26 +0700

    perf: improve open directory performance

--- src/Components/Functions/Loading/loading.ts
+++ src/Components/Functions/Loading/loading.ts
@@ -6,6 +6,14 @@ const startLoading = (): void => {
 	const LOADING_BAR = document.querySelector<HTMLElement>('.loading-bar');
 	LOADING_BAR.dataset.loading = 'true';
 };
+/**
+ * Check if the loading animation is running
+ * @returns {any}
+ */
+const isLoading = (): boolean => {
+	const LOADING_BAR = document.querySelector<HTMLElement>('.loading-bar');
+	return LOADING_BAR.dataset.loading === 'true';
+};
 
 /**
  * Stop the loading animation
@@ -16,4 +24,4 @@ const stopLoading = (): void => {
 	LOADING_BAR.dataset.loading = 'false';
 };
 
-export { startLoading, stopLoading };
+export { startLoading, stopLoading, isLoading };
