6301294a81776c87a9a362427da3692996991ce4
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2021-11-09 20:52:00 +0700

    fix: restoring file from trash not working handler

--- src/Api/trash.ts
+++ src/Api/trash.ts
@@ -4,6 +4,12 @@ import FileMetaData from '../Typings/fileMetaData';
 interface TrashData {
 	files: FileMetaData[];
 }
+
+interface ReturnInformation {
+	status: boolean;
+	message: string;
+	request_confirmation: boolean;
+}
 /**
  * Get list of files in trash
  *
@@ -29,24 +35,34 @@ const DeleteFiles = (paths: string[]): Promise<void> => {
 /**
  * Restore files/dirs from trash
  * @param {string[]} paths - Paths to be restored
- * @returns {Promise<void>} - Promise that resolves when files are restored
+ * @param {boolean} force - Force restore
+ * @returns {Promise<ReturnInformation>} - Promise that resolves when files are restored
  */
-const RestoreFiles = (paths: string[]): Promise<void> => {
+const RestoreFiles = async (paths: string[], force = false): Promise<ReturnInformation> => {
 	return new Promise((resolve) => {
-		invoke('restore_files', { paths }).then(() => resolve());
+		invoke('restore_files', { paths, force }).then((result) => resolve(result as ReturnInformation));
 	});
 };
 
+/**
+ * Delete files/dirs from trash permanently
+ * @param {string[]} paths
+ * @returns {Promise<void>}
+ */
 const PurgeFiles = (paths: string[]): Promise<void> => {
 	return new Promise((resolve) => {
 		invoke('purge_trashes', { paths }).then(() => resolve());
 	});
 };
 
-const RestoreFile = (original_parent: string, basename: string): Promise<void> => {
-	return new Promise((resolve) => {
-		invoke('restore_trash', { originalParent: original_parent, basename }).then(() => resolve());
-	});
+/**
+ * Restore a file according with original parent and basename known
+ * @param {string} original_parent
+ * @param {string} basename
+ * @returns {Promise<void>}
+ */
+const RestoreFile = async (original_parent: string, basename: string): Promise<void> => {
+	return await invoke('restore_trash', { originalParent: original_parent, basename });
 };
 
 export { getTrashedFiles, DeleteFiles, RestoreFiles, PurgeFiles, RestoreFile };
