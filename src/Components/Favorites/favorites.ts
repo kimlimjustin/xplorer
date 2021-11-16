fae50054fb5e2118e2a0e7432af471e197e9d688
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2021-11-16 16:37:20 +0700

    fix: pin image to favorites make Xplorer crashes on open

--- src/Components/Favorites/favorites.ts
+++ src/Components/Favorites/favorites.ts
@@ -4,6 +4,7 @@ import FavoritesAPI from '../../Api/favorites';
 import DirectoryAPI from '../../Api/directory';
 import Storage from '../../Api/storage';
 import defaultFavorites from './defaultFavorites';
+import FileAPI from '../../Api/files';
 let FavoritesData: FavoritesAPI;
 
 /**
@@ -20,26 +21,17 @@ const Favorites = async (): Promise<string> => {
 
 	let result = '<section class="home-section">';
 	result += `<h1 class="section-title">${await Translate('Favorites')}</h1>`;
-	const defaultFavoritesList = (await defaultFavorites()).map(
-		(favorite) => favorite.name
-	);
+	const defaultFavoritesList = (await defaultFavorites()).map((favorite) => favorite.name);
 	for (const favorite of favorites) {
 		if (favorite.path === 'xplorer://Home') continue;
 		let icon = favorite.icon;
 		if (!icon) {
 			if (defaultFavoritesList.indexOf(favorite.name) === -1) {
 				const isdir = await new DirectoryAPI(favorite.path).isDir();
-				icon = await fileThumbnail(
-					favorite.name,
-					isdir ? 'folder' : 'file',
-					false
-				);
+				const exists = await new FileAPI(favorite.path).exists();
+				icon = await fileThumbnail(exists ? favorite.path : favorite.name, isdir ? 'folder' : 'file', false);
 			} else {
-				icon = await fileThumbnail(
-					favorite.name.toLowerCase(),
-					'folder',
-					false
-				);
+				icon = await fileThumbnail(favorite.name.toLowerCase(), 'folder', false);
 			}
 		}
 		result += `<div
