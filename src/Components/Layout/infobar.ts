d500b2f67d6dd3cfd5f0fe84e868263650e8d953
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2021-11-22 21:28:43 +0700

    feat: initialize infobar feature

---
+++ src/Components/Layout/infobar.ts
@@ -0,0 +1,28 @@
+import { updateTheme } from '../Theme/theme';
+import Storage from '../../Api/storage';
+let infoBarElement: HTMLElement;
+
+const UpdateInfo = (key: string, value: string): void => {
+	const el = document.querySelector(`.infobar-item#${key}`);
+	if (!el) return;
+	el.innerHTML = value;
+};
+
+/**
+ * Initialize and create infobar element
+ * @returns {any}
+ */
+const Infobar = async (): Promise<void> => {
+	const appearance = await Storage.get('appearance');
+	if (!(appearance.showInfoBar ?? true)) return;
+	infoBarElement = document.createElement('div');
+	infoBarElement.classList.add('infobar');
+	infoBarElement.id = 'infobar';
+	infoBarElement.innerHTML = `
+	<div class="infobar-item" id="number-of-files"><div>
+	`;
+	document.querySelector('.main').appendChild(infoBarElement);
+	updateTheme('infobar');
+};
+export default Infobar;
+export { UpdateInfo };
