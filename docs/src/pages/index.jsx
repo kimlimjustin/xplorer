36f731fee01cbd767db49138948d731f0c1d0d03
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2021-11-24 14:28:28 -0500

    Added slide selection and fixed image overflow for small screen sizes

--- docs/src/pages/index.jsx
+++ docs/src/pages/index.jsx
@@ -14,6 +14,7 @@ export default function Home() {
 						<h1 className="banner--title">File Explorer.</h1>
 						<h1 className="banner--subtitle">Redefined.</h1>
 						<h2 className="banner--description">Free and Open Source Software. Runs everywhere.</h2>
+
 						<div className="padding-top--l padding-bottom--l">
 							<a className="btn margin--sm" href="https://github.com/kimlimjustin/xplorer/releases" target="_blank">
 								Download now
@@ -23,14 +24,17 @@ export default function Home() {
 							</Link>
 						</div>
 					</div>
+
 					<div className="col col--8">
 						<Slideshow />
 					</div>
 				</div>
+
 				<div className="padding-top--xl built-with-web padding-bottom--xl">
 					<h1>
 						Cross-platform File Explorer. <em>Powered by the web.</em>
 					</h1>
+
 					<span>
 						Built using the{' '}
 						<a href="https://tauri.studio" target="_blank">
@@ -46,17 +50,20 @@ export default function Home() {
 						</a>{' '}
 						for the backend, Xplorer promises you an unprecedented experience.
 					</span>
+
 					<div className="row padding-top--xl features--list">
 						<div className="col col--4 padding-left--m padding-right--m">
 							<img src="/img/home/designed-out-of-the-box.png" alt="Xplorer designed out of the box" />
 							<h2>Designed out of the box</h2>
 							<p>Say goodbye to the old design by traditional app and say hello to this simple yet powerful design.</p>
 						</div>
+
 						<div className="col col--4 padding-left--m padding-right--m">
 							<img src="/img/home/support-tabs.png" alt="Support tabs" />
 							<h2>Supports Multiple Tabs</h2>
 							<p>Xplorer helps you organize you files easier by supporting multiple tabs .</p>
 						</div>
+
 						<div className="col col--4 padding-left--m padding-right--m">
 							<img src="/img/home/preview-files.png" alt="" />
 							<h2>File Preview</h2>
