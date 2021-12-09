895dbd96f68cad1f1610e882a437c0c9870c4a5f
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2021-12-09 19:00:30 +0100

    Updated rootReducer typing

--- src/Store/Reducers/index.ts
+++ src/Store/Reducers/index.ts
@@ -1,7 +1,52 @@
 import { combineReducers } from 'redux';
+import { Actions } from '../../Typings/Store/store';
 
-const rootReducer = combineReducers({
-  test: (state = {}) => state
+import { IAppReducerState } from '../../Typings/Store/app';
+import { ICliReducerState } from '../../Typings/Store/cli';
+import { IDirectoryReducerState } from '../../Typings/Store/directory';
+import { IDriveReducerState } from '../../Typings/Store/drive';
+import { IFavoritesReducerState } from '../../Typings/Store/favorites';
+import { IFilesReducerState } from '../../Typings/Store/files';
+import { ILocalesReducerState } from '../../Typings/Store/locales';
+import { IPlatformReducerState } from '../../Typings/Store/platform';
+import { IStorageReducerState } from '../../Typings/Store/storage';
+import { IWindowReducerState } from '../../Typings/Store/window';
+
+import AppReducer from "./AppReducer";
+import CliReducer from "./CliReducer";
+import DirectoryReducer from "./DirectoryReducer";
+import DriveReducer from "./DriveReducer";
+import FavoritesReducer from "./FavoritesReducer";
+import FilesReducer from "./FilesReducer";
+import LocalesReducer from "./LocalesReducer";
+import PlatformReducer from "./PlatformReducer";
+import StorageReducer from "./StorageReducer";
+import WindowReducer from "./WindowReducer";
+
+export interface IAppState {
+  app: IAppReducerState,
+  cli: ICliReducerState,
+  directory: IDirectoryReducerState,
+  drive: IDriveReducerState,
+  favorites: IFavoritesReducerState,
+  files: IFilesReducerState,
+  locales: ILocalesReducerState,
+  platform: IPlatformReducerState,
+  storage: IStorageReducerState,
+  window: IWindowReducerState
+}
+
+const rootReducer = combineReducers<IAppState, Actions>({
+  app: AppReducer,
+  cli: CliReducer,
+  directory: DirectoryReducer,
+  drive: DriveReducer,
+  favorites: FavoritesReducer,
+  files: FilesReducer,
+  locales: LocalesReducer,
+  platform: PlatformReducer,
+  storage: StorageReducer,
+  window: WindowReducer
 });
 
 export default rootReducer;
--- src/Store/index.ts
+++ src/Store/index.ts
@@ -3,13 +3,13 @@ import createSagaMiddleware from 'redux-saga';
 import { createLogger } from 'redux-logger';
 
 import rootSaga from './Sagas';
-import rootReducer from './Reducers';
+import rootReducer, { IAppState } from './Reducers';
 import { Actions } from '../Typings/Store/store';
 
 const sagaMiddleware = createSagaMiddleware();
 const loggerMiddleware = createLogger();
 
-const store = createStore<ReturnType<typeof rootReducer>, Actions, unknown, unknown>(
+const store = createStore<IAppState, Actions, unknown, unknown>(
   rootReducer,
   compose(
     applyMiddleware(
