9929f35c922ae969817301a22f491eda02363300
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2021-12-10 17:43:12 +0700

    fix: app crashes when favorites path is null

--- src/Api/favorites.ts
+++ src/Api/favorites.ts
@@ -9,13 +9,19 @@ class FavoritesAPI {
 	VIDEO_PATH: string;
 	HOMEDIR_PATH: string;
 	async build(): Promise<void> {
-		this.DOCUMENT_PATH = await path.documentDir();
-		this.DOWNLOAD_PATH = await path.downloadDir();
-		this.DESKTOP_PATH = await path.desktopDir();
-		this.PICTURE_PATH = await path.pictureDir();
-		this.MUSIC_PATH = await path.audioDir();
-		this.VIDEO_PATH = await path.videoDir();
-		this.HOMEDIR_PATH = await path.homeDir();
+		try {
+			this.DOCUMENT_PATH = await path.documentDir();
+			this.DOWNLOAD_PATH = await path.downloadDir();
+			this.DESKTOP_PATH = await path.desktopDir();
+			this.PICTURE_PATH = await path.pictureDir();
+			this.MUSIC_PATH = await path.audioDir();
+			this.VIDEO_PATH = await path.videoDir();
+			// eslint-disable-next-line no-empty
+		} catch (_) {}
+		try {
+			this.HOMEDIR_PATH = await path.homeDir();
+			// eslint-disable-next-line no-empty
+		} catch (_) {}
 	}
 }
 
