0dc0b6b6c9b5e3d9d4d8370434515f496ebb49bb
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2021-12-26 19:51:11 +0700

    refactor<docs>: redesign docs site

---
+++ docs/src/util/icon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+export function Check() {
+	return (
+		<svg viewBox="0 0 24 24">
+			<path
+				d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8l-1.41-1.42z"
+				fill="currentColor"
+			></path>
+		</svg>
+	);
+}
