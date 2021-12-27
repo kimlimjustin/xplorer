41649ecd2c0a11808ce3b235f6a9f339002e72cc
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2021-12-27 22:34:09 +0700

    feat<docs>: add contributors and newsletter section

--- docs/src/util/icon.jsx
+++ docs/src/util/icon.jsx
@@ -9,3 +9,19 @@ export function Check() {
 		</svg>
 	);
 }
+
+export const EmailIcon = () => {
+	return (
+		<svg width="1em" height="1em" viewBox="0 0 24 24" className="email-icon">
+			<path d="M20 8l-8 5l-8-5V6l8 5l8-5m0-2H4c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z" fill="currentColor"></path>
+		</svg>
+	);
+};
+
+export const ErrorIcon = () => {
+	return (
+		<svg viewBox="0 0 24 24">
+			<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z" fill="currentColor"></path>
+		</svg>
+	);
+};
