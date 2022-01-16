068286daa8721baf7467b3ba350269ebffb26ac3
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2022-01-16 14:53:46 +0700

    feat: basic searching file in browser

--- src/Util/constants.ts
+++ src/Util/constants.ts
@@ -1,3 +1,5 @@
 export const SERVICE_ENDPOINT = 'http://localhost:3030/';
 export const READ_DIR_ENDPOINT = SERVICE_ENDPOINT + 'read_dir?path=';
 export const GET_FAVORITES_PATH_ENDPOINT = SERVICE_ENDPOINT + 'dir/favorites';
+export const CHECK_EXIST_ENDPOINT = SERVICE_ENDPOINT + 'check_exist?path=';
+export const CHECK_ISDIR_ENDPOINT = SERVICE_ENDPOINT + 'check_isdir?path=';
