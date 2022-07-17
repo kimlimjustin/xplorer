730c7177816986c2376537adce46a12510e9fc4e
Justin Maximillian Kimlim <kimlimjustin@gmail.com>
2022-07-17 22:27:06 +0700

    fix: not working on windows under windows 11 due to mica requirement

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use sysinfo::{System, SystemExt};
 
 pub fn read_to_serde_json(path: PathBuf) -> serde_json::Value {
     let file: Result<serde_json::Value, serde_json::Error> =
@@ -17,6 +18,15 @@ macro_rules! json_value_or_null {
     ($json:expr, $get:expr) => {
         $json.get($get).unwrap_or(&serde_json::Value::Null)
     };
+}
+
+
+pub fn is_win_11() -> bool {
+    let sys = System::new_all();
+    let version = sys.os_version().unwrap();
+    let version = version.split('(').collect::<Vec<&str>>()[1].split(')').collect::<Vec<&str>>()[0];
+    let version: u32 = version.split('.').collect::<Vec<&str>>()[0].parse().unwrap();
+    version >= 22000
 }
 
 // TODO: Any way to export macro without this lane?
